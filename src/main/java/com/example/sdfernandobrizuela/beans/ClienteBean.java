e94c8a53ddf6ff7c56ae11c5ebd4402591202175
fernando <ffba97@gmail.com>
2023-10-24 23:25:29 -0300

    Actualizados beans

--- src/main/java/com/example/sdfernandobrizuela/beans/ClienteBean.java
+++ src/main/java/com/example/sdfernandobrizuela/beans/ClienteBean.java
@@ -1,14 +1,13 @@
 package com.example.sdfernandobrizuela.beans;
 
 import com.example.sdfernandobrizuela.abstracts.AbstractBean;
-import com.example.sdfernandobrizuela.interfaces.IClienteBean;
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
+import lombok.Data;
 
 @Entity
-@Table(name = "cliente")
-public class ClienteBean extends AbstractBean implements IClienteBean {
+@Data
+@Table(name = "clientes")
+public class ClienteBean extends AbstractBean {
     @Column
     private String nombre;
     @Column
@@ -19,53 +18,7 @@ public class ClienteBean extends AbstractBean implements IClienteBean {
     private String telefono;
     @Column
     private String email;
-    @Column
-    private String numeroTarjeta;
-
-    public String getNombre() {
-        return nombre;
-    }
-
-    public void setNombre(String nombre) {
-        this.nombre = nombre;
-    }
-    public String getRuc() {
-        return ruc;
-    }
-    public void setRuc(String ruc) {
-        this.ruc = ruc;
-    }
-
-    public String getCedula() {
-        return cedula;
-    }
-
-    public void setCedula(String cedula) {
-        this.cedula = cedula;
-    }
-
-    public String getTelefono() {
-        return telefono;
-    }
-
-    public void setTelefono(String telefono) {
-        this.telefono = telefono;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public String getNumeroTarjeta() {
-        return numeroTarjeta;
-    }
-
-    public void setNumeroTarjeta(String numeroTarjeta) {
-        this.numeroTarjeta = numeroTarjeta;
-    }
+//    @OneToOne
+//    private ClienteDetalleBean detalleBean;
 
 }
