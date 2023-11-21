c2fcf6ef873e669d44ae657270c7fb1450986cea
Fernando Brizuela <ffba97@gmail.com>
2023-11-21 15:46:53 -0300

    Corrección autenticación

--- src/main/java/com/example/sdfernandobrizuela/auth/AuthController.java
+++ src/main/java/com/example/sdfernandobrizuela/auth/AuthController.java
@@ -3,6 +3,7 @@ package com.example.sdfernandobrizuela.auth;
 import com.example.sdfernandobrizuela.auth.requests.AuthRequest;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PostAuthorize;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -15,6 +16,7 @@ public class AuthController {
     @Autowired
     private AuthenticationService authenticationService;
 
+    @PostAuthorize("hasRole('ADMIN')")
     @PostMapping("/register")
     public ResponseEntity registrar(@RequestBody RegisterRequest request){
         return ResponseEntity.ok(authenticationService.registrar(request));
