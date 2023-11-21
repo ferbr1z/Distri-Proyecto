752e208e98144bf052be5a973f9625f739c5736b
Fernando Brizuela <ffba97@gmail.com>
2023-11-21 15:49:49 -0300

    Se aumentó el tiempo de vida del token

--- src/main/java/com/example/sdfernandobrizuela/auth/jwt/JWTService.java
+++ src/main/java/com/example/sdfernandobrizuela/auth/jwt/JWTService.java
@@ -22,7 +22,7 @@ public class JWTService implements IJWTService {
         return Jwts.builder()
                 .subject(userDetails.getUsername())
                 .issuedAt(new Date(System.currentTimeMillis()))
-                .expiration(new Date(System.currentTimeMillis() + 1000 * 60 * 24))
+                .expiration(new Date(System.currentTimeMillis() + 1000 * 60 * 60 * 24))
                 .signWith(getSigningKey(), SignatureAlgorithm.HS256)
                 .compact();
     }
